390fd38083e611b788a8c99f434615e3bac1882d
Timothy Brahm <timothy.david.brahm@gmail.com>
2020-01-13 20:59:09 -0500

    pre overhaul

--- game.js
+++ game.js
@@ -3,11 +3,17 @@ const mysql = require("mysql");
 const { printTable } = require('console-table-printer');
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
+
     user: "root",
     password: "root",
-    database: "dnd_db"
+    database: "dnd_db",
+
+    user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME
+
 });
 
 connection.connect(function (err) {
